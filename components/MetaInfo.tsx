9031f5bb8f8f69cfd75527166efded01b50002e5
ihmoon <ihmoon>
2023-01-18 15:14:05 +0900

    첫번째 세팅

---
+++ components/MetaInfo.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Head from 'next/head';
+
+const MetaInfo = () => {
+  return (
+    <Head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="css/style.css" />
+        {/* <script src="js/script.js" defer></script> */}
+        <title>SpaceX</title>
+    </Head>
+
+  )
+}
+
+export default MetaInfo
